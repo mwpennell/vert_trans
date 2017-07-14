d7fed09171e2cd581f4adedc7ad3e12fa3fe4066
Matthew Pennell <mwp@dhcp-206-87-192-24.ubcsecure.wireless.ubc.ca>
2017-07-14 15:05:56 -0700

    added some little helper fxns

--- R/util.R
+++ R/util.R
@@ -14,6 +14,58 @@ phyndr_treedata <- function(x, n){
   td_set
 }
 
+## function for getting 95% HPD from mcmc.samples
+hdr <- diversitree:::hdr
+
+## function for determining what proportion of 95% HPD overlaps with 0
+z.hdr <- function(x){
+  hpd <- hdr(x)
+  ## all samples in 95% HPD
+  nf <- which(which(x >= hpd.lim[1]) %in% which(x <= hpd.lim[2]))
+  ## lower than 0
+  l <- length(which(x[nf] < 0))/length(x)
+  ## greater than 0
+  g <- length(which(x[nf] > 0))/length(x)
+  list(lower.than.zero=l, greater.than.zero=g)
+}
+
+cache.mcmc <- function(x)
+  diversitree:::get.cache(get.likelihood(x))
+
+
+## function for summarizing table for xy 2 zw analysis
+build.tab.xz <- function(cache){
+  st <- cache$states
+  
+  mhom <- length(intersect(which(st[,1] == 0), which(st[,2] == 0)))
+  mhet <- length(intersect(which(st[,1] == 1), which(st[,2] == 0)))
+  fhom <- length(intersect(which(st[,1] == 0), which(st[,2] == 1)))
+  fhet <- length(intersect(which(st[,1] == 1), which(st[,2] == 1)))
+  uhom <- length(intersect(which(st[,1] == 0), which(is.na(st[,2]))))
+  
+  dat <- c(mhom, mhet, fhom, fhet, uhom)
+  names(dat) <- c("male.hom", "male.het", "fem.hom", "fem.het", "unk.hom")
+  dat
+}
+
+trans.multitrait <- function(x){
+  cc <- cache.mcmc(x)
+  lik <- make.musse.multitrait(cc$info$phy, data.frame(cc$states),
+                               depth=c(0,0,1))
+  r <- lapply(seq_len(nrow(x)), function(i) get.pars(x[i,c(2:11)], lik))
+  r <- do.call(rbind,r)
+  as.data.frame(r)
+}
+
+get.pars <- function(p, lik){
+  p.full <- lik(as.numeric(p), pars.only=TRUE)
+  p.full[grepl("^q", names(p.full))]
+}
+
+unfactor <- function(x)
+  as.numeric(levels(x)[as.integer(x)])
+
+
 
 
 
